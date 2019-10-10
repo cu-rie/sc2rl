d8e1f8227a6c6160bfae2ab9fda9b06aebe5d9db
junyoung.park <junyoungpark@kaist.ac.kr>
2019-10-10 22:30:24 +0900

    complete decouple env-agent depedency

---
+++ sc2rl/environments/MicroTestEnvironment.py
@@ -0,0 +1,85 @@
+from time import sleep
+
+from sc2 import Race
+from sc2.player import Bot
+from sc2rl.environments.EnvironmentBase import SC2EnvironmentBase
+from sc2rl.environments.SC2BotAI import DumbSC2BotAI
+
+
+class MicroTestEnvironment(SC2EnvironmentBase):
+
+    def __init__(self, map_name, reward_func, state_proc_func, realtime=False):
+        allies = Bot(Race.Terran, DumbSC2BotAI())
+        super(MicroTestEnvironment, self).__init__(map_name=map_name,
+                                                   allies=allies,
+                                                   realtime=realtime)
+
+        self.reward_func = reward_func
+        self.state_proc_func = state_proc_func
+
+    def reset(self):
+        sc2_game_state = self._reset()
+        return self.state_proc_func(sc2_game_state)
+
+    def observe(self):
+        sc2_game_state = self._observe()
+        return self.state_proc_func(sc2_game_state)
+
+    def step(self, action_args):
+        sc2_cur_state = self.observe()
+        sc2_next_state, _ = self._step(action_args=action_args)
+
+        # additional routine for checking done!
+        next_num_allies = len(sc2_next_state.units.owned)
+        next_num_enemies = len(sc2_next_state.units.enemy)
+
+        done = (next_num_allies == 0 or next_num_enemies == 0)
+
+        cur_state = self.state_proc_func(sc2_cur_state)
+        next_state = self.state_proc_func(sc2_next_state)
+        reward = self.reward_func(cur_state, next_state)
+
+        if done:  # Burn few remaining frames
+            self.burn_last_frames()
+
+        return next_state, reward, done
+
+    def burn_last_frames(self):
+        while True:
+            sc2_cur_state = self._observe()
+            next_num_allies = len(sc2_cur_state.units.owned)
+            next_num_enemies = len(sc2_cur_state.units.enemy)
+
+            if not (next_num_allies == 0 or next_num_enemies == 0):
+                break
+            else:
+                _, _ = self._step(action_args=None)
+
+
+if __name__ == "__main__":
+
+    def reward_func(s, ns):
+        return 1
+
+
+    def state_proc_func(s):
+        return 1
+
+
+    map_name = "training_scenario_1"
+    test_reward_func = reward_func
+    test_sate_proc_func = state_proc_func
+
+    env = MicroTestEnvironment(map_name, test_reward_func, test_sate_proc_func)
+    done_cnt = 0
+
+    while True:
+        next_state, reward, done = env.step(action_args=1)
+        if done:
+            done_cnt += 1
+            if done >= 10:
+                break
+
+    env.close()
+
+    print("We are in the end game.")
---
+++ sc2rl/environments/SC2BotAI.py
@@ -0,0 +1,36 @@
+import sc2
+
+
+class SC2BotAIBase(sc2.BotAI):
+    """
+        An abstract class for interfacing RL agent and sc2.BotAI
+    """
+
+    def __init__(self):
+        super(SC2BotAIBase, self).__init__()
+
+    async def on_step(self, action_args):
+        """
+        GET action_args from RL agent side and process the input to the SC2 commands!
+
+        #expected behaviour#
+        if action_args is None: # for supporting non-controllable states.
+            action_list = []
+        else:
+            action_list = processing_inputs(action_args)
+
+        await self.do_actions(action_list)
+
+        """
+
+        raise NotImplementedError
+
+
+class DumbSC2BotAI(SC2BotAIBase):
+    """
+        an dumb agent that do nothing
+    """
+
+    async def on_step(self, s):
+        action_list = []
+        await self.do_actions(action_list)
