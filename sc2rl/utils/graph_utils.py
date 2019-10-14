2071e9d765f42aef3a51411f881ed2505440b236
junyoung.park <junyoungpark@kaist.ac.kr>
2019-10-14 13:52:50 +0900

    add hetero_graph_pop and set utils

--- sc2rl/utils/graph_utils.py
+++ sc2rl/utils/graph_utils.py
@@ -1,4 +1,3 @@
-
 def get_batched_index(batched_graph, index_list, return_num_targets=False):
     _num_nodes = 0
     return_indices = []
@@ -16,3 +15,15 @@ def get_batched_index(batched_graph, index_list, return_num_targets=False):
         return return_indices
     else:
         return return_indices, num_targets
+
+
+def pop_node_feature_dict(graph, node_feature_key='node_feature'):
+    ret_dict = dict()
+    for ntype in graph.ntpyes:
+        ret_dict[ntype] = graph.nodes[ntype].data.pop(node_feature_key)
+    return ret_dict
+
+
+def set_node_feature_dict(graph, feature_dict, node_feature_key='node_feature'):
+    for key, val in feature_dict.items():
+        graph.nodes[key].data[node_feature_key] = val
