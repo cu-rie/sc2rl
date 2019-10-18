414e8943c9065923ca9960a346e03738f3ee73dc
junyoung.park <junyoungpark@kaist.ac.kr>
2019-10-18 19:54:32 +0900

    w.i.p

--- sc2rl/ruuners/MultiStepActorRunner.py
+++ sc2rl/ruuners/MultiStepActorRunner.py
@@ -1,9 +1,13 @@
 from sc2rl.ruuners.RunnerBase import RunnerBase
+from sc2rl.utils.HistoryManagers import HistoryManager
+from
 
-
-class MultiStepActorRuner(RunnerBase):
+class MultiStepActorRunner(RunnerBase):
 
     def __init__(self, env, agent, sample_spec, n_steps):
-        super(MultiStepActorRuner, self).__init__(env=env, agent=agent, sample_spec=sample_spec)
+        super(MultiStepActorRunner, self).__init__(env=env, agent=agent, sample_spec=sample_spec)
+        self.history_manager = HistoryManager(n_hist_steps=n_steps, init_graph=None)
 
-    def 
+    def run_1_episode(self):
+
+        self.history_manager.reset()
