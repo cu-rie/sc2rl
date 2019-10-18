5b3330086f889155bcb3a3eb801faab312d9384c
junyoung.park <junyoungpark@kaist.ac.kr>
2019-10-19 01:31:21 +0900

    Double critic + target net implemented
    1. Decouple all dependencies among the critic and actor
    2. Double critic supported
    3. Target net supported

--- sc2rl/rl/brains/MultiStepInputActorCriticBrain_refac.py
+++ sc2rl/rl/brains/MultiStepInputActorCriticBrain_refac.py
@@ -1,39 +1,156 @@
 import torch
 from sc2rl.rl.brains.BrainBase import BrainBase
 from sc2rl.config.ConfigBase import ConfigBase
+from sc2rl.config.nn_configs import VERY_SMALL_NUMBER
+from sc2rl.utils.graph_utils import get_filtered_node_index_by_type, get_index_mapper
+
 
 class MultiStepActorCriticBrainConfig(ConfigBase):
 
     def __init__(self,
-                 MultiStepSha):
-
+                 MultiStepActorCriticConfig=None):
+        self.MultiStepActorCriticConfig
 
 
 class MultiStepActorCriticBrain(BrainBase):
 
-    def __init__(self, actor, critic, critic_target=None, critic2=None,
-                 hyper_params):
+    def __init__(self, actor, critic, hyper_params,
+                 critic_target=None, critic2=None, critic2_target=None):
         super(MultiStepActorCriticBrain, self).__init__()
         self.actor = actor
+
         self.critic = critic
         self.critic_target = critic_target
+
         self.critic2 = critic2
+        self.critic2_target = critic2_target
+
+        self.hyper_params = hyper_params
+
+        optimizer = self.get_optimizer(hyper_params['optimizer'])
+        self.actor_optimizer = optimizer(self.actor.parameters(), lr=hyper_params['actor_lr'])
+        self.critic_optimizer = optimizer(self.critic.parameters(), lr=hyper_params['critic_lr'])
 
         if critic2 is not None:
             self.double_q = True
+            self.critic2_optimizer = optimizer(self.critic2.parameters(), lr=hyper_params['critic_lr'])
         else:
             self.double_q = False
 
-        if critic_target is not None:
-            self.q_targeted = True
-        else:
-            self.q_targeted = False
+    def fit_critic(self,
+                   num_time_steps,
+                   c_hist_graph, c_hist_feature,
+                   c_curr_graph, c_curr_feature,
+                   c_maximum_num_enemy,
+                   action,
+                   n_hist_graph, n_hist_feature,
+                   n_curr_graph, n_curr_feature,
+                   n_maximum_num_enemy,
+                   rewards,
+                   dones):
 
-    def fit_critic(self):
-        pass
+        fit_dict = dict()
 
-    def fit_actor(self):
-        pass
+        loss = self.compute_critic_loss(self.critic,
+                                        num_time_steps,
+                                        c_hist_graph, c_hist_feature,
+                                        c_curr_graph, c_curr_feature,
+                                        c_maximum_num_enemy,
+                                        action,
+                                        n_hist_graph, n_hist_feature,
+                                        n_curr_graph, n_curr_feature,
+                                        n_maximum_num_enemy,
+                                        rewards,
+                                        dones,
+                                        target_net=self.critic_target)
+
+        self.clip_and_optimize(self.critic_optimizer, self.critic.parameters(), loss,
+                               clip_val=self.hyper_params['critic_norm_clip_val'])
+
+        self.update_target_network(self.hyper_params['tau'], self.critic, self.critic_target)
+
+        fit_dict['critic_loss'] = loss.detach().cpu().numpy()
+
+        if self.double_q:
+            loss2 = self.compute_critic_loss(self.critic2,
+                                             num_time_steps,
+                                             c_hist_graph, c_hist_feature,
+                                             c_curr_graph, c_curr_feature,
+                                             c_maximum_num_enemy,
+                                             action,
+                                             n_hist_graph, n_hist_feature,
+                                             n_curr_graph, n_curr_feature,
+                                             n_maximum_num_enemy,
+                                             rewards,
+                                             dones,
+                                             target_net=self.critic_target2)
+
+            self.clip_and_optimize(self.critic2_optimizer, self.critic2.parameters(), loss2,
+                                   clip_val=self.hyper_params['critic_norm_clip_val'])
+
+            self.update_target_network(self.hyper_params['tau'], self.critic2, self.critic_target2)
+            fit_dict['critic_loss2'] = loss2.detach().cpu().numpy()
+
+        return fit_dict
+
+    def compute_critic_loss(self,
+                            critic_net,
+                            num_time_steps,
+                            c_hist_graph, c_hist_feature,
+                            c_curr_graph, c_curr_feature,
+                            c_maximum_num_enemy,
+                            action,
+                            n_hist_graph, n_hist_feature,
+                            n_curr_graph, n_curr_feature,
+                            n_maximum_num_enemy,
+                            rewards,
+                            dones,
+                            target_net=None):
+
+        if target_net is None:
+            target_net = critic_net
+
+        # cur_q : [#. current ally units x #. actions]
+        cur_q = self.get_q(critic_net,
+                           num_time_steps,
+                           c_hist_graph, c_hist_feature,
+                           c_curr_graph, c_curr_feature, c_maximum_num_enemy)
+
+        # cur_q : [#. current ally units]
+        cur_q = cur_q.gather(-1, action.unsqueeze(-1)).squeeze(dim=-1)
+
+        # The number of allies in the current (batched) graph may differ from the one of the next graph
+        target_q = torch.zeros_like(cur_q)
+
+        with torch.no_grad():
+            exp_target_q, entropy = self.get_exp_q(target_net,
+                                                   num_time_steps,
+                                                   n_hist_graph, n_hist_feature,
+                                                   n_curr_graph, n_curr_feature,
+                                                   n_maximum_num_enemy)
+
+        unsorted_target_q = exp_target_q + self.entropy_coeff * entropy  # [#. next ally_units]
+        cur_idx, next_idx = get_index_mapper(c_curr_graph, n_curr_graph)
+        target_q[cur_idx] = unsorted_target_q[next_idx]
+        target_q = rewards + self.gamma * target_q * (1 - dones)
+        loss = torch.nn.functional.mse_loss(input=cur_q, target=target_q)
+        return loss
+
+    def fit_actor(self,
+                  num_time_steps,
+                  hist_graph, hist_feature,
+                  curr_graph, curr_feature, maximum_num_enemy):
+
+        loss = self.compute_actor_loss(num_time_steps,
+                                       hist_graph, hist_feature,
+                                       curr_graph, curr_feature, maximum_num_enemy)
+
+        self.clip_and_optimize(self.actor_optimizer, self.actor.parameters(), loss,
+                               clip_val=self.hyper_params['actor_nomr_clip_val'])
+
+        fit_dict = dict()
+        fit_dict['actor_loss'] = loss.detach().cpu().nump()
+        return fit_dict
 
     def compute_actor_loss(self,
                            num_time_steps,
@@ -56,7 +173,13 @@ class MultiStepActorCriticBrain(BrainBase):
                                              hist_graph, hist_feature,
                                              curr_graph, curr_feature, maximum_num_enemy)
 
+        log_p_move = prob_dict['log_p_move']
+        log_p_hold = prob_dict['log_p_hold']
+        log_p_attack = prob_dict['log_p_attack']
+
+        log_ps = torch.cat([log_p_move, log_p_hold, log_p_attack], dim=1)
         ps = prob_dict['probs']
+
         vs = (ps * qs).sum(1).deatch()
         policy_target = qs - vs
 
@@ -65,9 +188,26 @@ class MultiStepActorCriticBrain(BrainBase):
             policy_target2 = qs - vs2
             policy_target = torch.min(policy_target, policy_target2)
 
+        unmasked_loss = log_ps * (self.log_alpha.exp() * log_ps - policy_target)
+        loss_mask = (log_ps > torch.log(torch.tensor(VERY_SMALL_NUMBER))).float()
+        loss = (unmasked_loss * loss_mask).sum() / loss_mask.sum()
+        return loss
 
+    def fit_alpha(self,
+                  num_time_steps,
+                  hist_graph, hist_feature,
+                  curr_graph, curr_feature, maximum_num_enemy):
 
+        prob_dict = self.actor.compute_probs(num_time_steps,
+                                             hist_graph, hist_feature,
+                                             curr_graph, curr_feature, maximum_num_enemy)
 
+        log_ps = prob_dict['log_ps']
+
+        alpha_loss = (-self.log_alpha * (log_ps * self.target_entropy).detach()).mean()
+        self.alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.alpha_optimizer.step()
 
     @staticmethod
     def get_q(critic_net,
@@ -94,7 +234,7 @@ class MultiStepActorCriticBrain(BrainBase):
         prob_dict = self.actor.compute_probs(num_time_steps,
                                              hist_graph, hist_feature,
                                              curr_graph, curr_feature, maximum_num_enemy)
-
+        entropy = prob_dict['ally_entropy']
         p = prob_dict['probs']
         exp_q = (q * p).sum(dim=1)
-        return exp_q
+        return exp_q, entropy
